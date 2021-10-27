dc7965ce44128669eab41b6c10cbd2406bcce93e
thanhdo <billslim0996@gmail.com>
2021-10-27 17:41:04 +0700

    lock by MAC

--- BarcodeGenerator/src/BarcodeGenerator.java
+++ BarcodeGenerator/src/BarcodeGenerator.java
@@ -1,19 +1,38 @@
 import javafx.application.Application;
-import javafx.print.PageLayout;
-import javafx.print.PageOrientation;
-import javafx.print.Paper;
-import javafx.print.PrinterJob;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import ui.MainPane;
 import user_interface.windows.WindowRegular;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Enumeration;
+
 public class BarcodeGenerator extends Application {
-    public static void main(String[] args) {
-        System.setProperty("configFileName", "config.txt");
-        launch(args);
+    public static void main(String[] args) throws SocketException {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        String macAddress = "";
+        while (networkInterfaces.hasMoreElements()) {
+            NetworkInterface ni = networkInterfaces.nextElement();
+            byte[] hardwareAddress = ni.getHardwareAddress();
+            if (hardwareAddress != null) {
+                String[] hexadecimalFormat = new String[hardwareAddress.length];
+                for (int i = 0; i < hardwareAddress.length; i++) {
+                    hexadecimalFormat[i] = String.format("%02X", hardwareAddress[i]);
+                }
+                macAddress = String.join("-", hexadecimalFormat);
+            }
+        }
+
+        if (macAddress.equals("F0-2F-74-F4-51-A")) {
+            System.setProperty("configFileName", "config.txt");
+            launch(args);
+        } else {
+            System.exit(1);
+        }
     }
 
     public void start(Stage stage) {
